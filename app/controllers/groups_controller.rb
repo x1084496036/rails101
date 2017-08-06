d0a1167938e0171ad776b595f3989ed7ce5d554e
palx <x1084496036@sina.com>
2017-08-07 01:08:09 +0800

    implement groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,4 +7,15 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def create
+    @group = Group.new(group_params)
+    @group.save
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title,:description)
+  end
+
 end
