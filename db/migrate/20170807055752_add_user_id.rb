b53cf609aba64b26b37ddae0919f1363671dfb58
palx <x1084496036@sina.com>
2017-08-07 14:07:13 +0800

    add user_id to group

---
+++ db/migrate/20170807055752_add_user_id.rb
@@ -0,0 +1,4 @@
+class AddUserId < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807041925) do
+ActiveRecord::Schema.define(version: 20170807055752) do
 
   create_table "groups", force: :cascade do |t|
     t.string "title"
