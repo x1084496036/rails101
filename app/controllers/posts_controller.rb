91bbc738530970d0e150b32e1f21da0d15c36855
palx <x1084496036@sina.com>
2017-08-08 13:11:07 +0800

    add my posts Edit && Delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!,:only => [:new,:create]
-
+  before_action :authenticate_user!
+  before_action :find_group_and_check_permission, only: [:edit,:update,:destroy]
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -11,7 +11,6 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.group = @group
     @post.user = current_user
-
     if @post.save
       redirect_to group_path(@group)
     else
@@ -19,10 +18,35 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = @post.group
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to account_posts_path,notice: "Update success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to account_posts_path,notice: "Delete success"
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def find_group_and_check_permission
+    @post = Post.find(params[:id])
+
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no permission."
+    end
   end
 
 end
